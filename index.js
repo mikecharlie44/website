97f7dfbaf090ce283b7bd301e039d9d8bdfcb350
Sabin swar <105520322+mikecharlie44@users.noreply.github.com>
2024-03-02 14:15:25 +0545

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener("mousemove", function(dets) {
     crsr.style.left = dets.x+"px"
     crsr.style.top = dets.y+"px"
     blur.style.left = dets.x -250 +"px"
-    blur.style.top = dets.y- 250 +"px"
+    blur.style.top = dets.y -250 +"px"
     
 })
 
